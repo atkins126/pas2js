c13ff84b58da7dad814d955f5332053d34337078
mattias <mattias@39b5ca93-38c5-41e0-b8fa-b9ac19d477b3>
2019-08-28 11:31:55 +0000

    tagged version 1.4.12

--- packages/pushjs/pushjs.js
+++
@@ -1,6 +0,0 @@
-﻿rtl.module("PushJS",["System","JS"],function () {
-  "use strict";
-  var $mod = this;
-  $mod.$rtti.$RefToProcVar("TPushFunction",{procsig: rtl.newTIProcSig(null)});
-  $mod.$rtti.$RefToProcVar("TPushParamsFallback",{procsig: rtl.newTIProcSig([["payload",$mod.$rtti["TPushFallbackPayload"]]])});
-});
