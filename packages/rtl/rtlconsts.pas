d19fbfe79575f72c604ae7ccaafc482028a585d8
mattias <mattias@39b5ca93-38c5-41e0-b8fa-b9ac19d477b3>
2020-07-08 12:09:34 +0000

    tagged version 1.4.30

--- packages/rtl/rtlconsts.pas
+++ packages/rtl/rtlconsts.pas
@@ -14,17 +14,15 @@ unit RTLConsts;
 
 interface
 
-Resourcestring
+const
   SArgumentMissing              = 'Missing argument in format "%s"';
   SInvalidFormat                = 'Invalid format specifier : "%s"';
   SInvalidArgIndex              = 'Invalid argument index in format: "%s"';
   SListCapacityError            = 'List capacity (%s) exceeded.';
   SListCountError               = 'List count (%s) out of bounds.';
-  SMapKeyError                  = 'Key not found : %s';
   SListIndexError               = 'List index (%s) out of bounds';
   SSortedListError              = 'Operation not allowed on sorted list';
-  SDuplicateString              = 'String list does not allow duplicates';
-  SDuplicateItem                = 'ThreadList does not allow duplicates';
+  SDuplicateString              = 'String list does not allow duplicates';  
   SErrFindNeedsSortedList       = 'Cannot use find on unsorted list';
 
   SInvalidName                  = 'Invalid component name: "%s"';
@@ -39,7 +37,6 @@ Resourcestring
   SCantWritePropertyS           = 'Cannot write property "%s"';
   SErrPropertyNotFound          = 'Unknown property: "%s"';
   SIndexedPropertyNeedsParams   = 'Indexed property "%s" needs parameters';
-  SErrInvalidTypecast           = 'Invalid class typecast';
 
   SErrInvalidInteger            = 'Invalid integer value: "%s"';
   SErrInvalidFloat              = 'Invalid floating-point value: "%s"';
@@ -55,31 +52,6 @@ Resourcestring
   SErrInvalidHourMinuteSecMsec  = '%d:%d:%d.%d is not a valid time specification';
 
   SInvalidGUID                  = '"%s" is not a valid GUID value';
-
-  SEmptyStreamIllegalReader       = 'Illegal Nil stream for TReader constructor';
-  SInvalidPropertyValue           = 'Invalid value for property';
-  SInvalidImage                   = 'Invalid stream format';
-  SUnknownProperty                = 'Unknown property: "%s"';
-  SUnknownPropertyType            = 'Unknown property type %d';
-  SAncestorNotFound               = 'Ancestor class for "%s" not found.';
-  SUnsupportedPropertyVariantType = 'Unsupported property variant type %d';
-  SPropertyException              = 'Error reading %s%s%s: %s';
-  SInvalidPropertyPath            = 'Invalid property path';
-  SReadOnlyProperty               = 'Property is read-only';
-  SClassNotFound                  = 'Class "%s" not found';
-
-  SEmptyStreamIllegalWriter       = 'Illegal Nil stream for TWriter constructor';
-  SErrInvalidPropertyType         = 'Invalid property type from streamed property: %d';
-  SParserExpected                 = 'Wrong token type: %s expected';
-  SParserInvalidFloat             = 'Invalid floating point number: %s';
-  SParserInvalidInteger           = 'Invalid integer number: %s';
-  SParserUnterminatedString       = 'Unterminated string';
-  SParserWrongTokenType           = 'Wrong token type: %s expected but %s found';
-  SParserWrongTokenSymbol         = 'Wrong token symbol: %s expected but %s found';
-  SParserLocInfo                  = ' (at %d,%d, stream offset %.8x)';
-  SParserUnterminatedBinValue     = 'Unterminated byte value';
-  SParserInvalidProperty          = 'Invalid property';
-  SRangeError                     = 'Range error';
 
 implementation
 
